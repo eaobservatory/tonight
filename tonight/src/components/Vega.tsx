7df54cb332b2c4493258a9f9dcea4865f3be1a65
katiejl <katieliu2@live.com>
2023-08-09 20:52:26 -0700

    more vega testing

--- tonight/src/components/Vega.tsx
+++ tonight/src/components/Vega.tsx
@@ -1,5 +1,17 @@
 import * as vega from "vega";
 import * as lite from "vega-lite";
+import { jcmtwx, jcmtnama } from "@/constants/figureGroups";
+import { getPV } from "@/utils/engarchive";
+
+const getUTCHour = (date: string) => {
+  console.log(typeof date);
+  let utcHour = Number(date.substring(11, 13)) + 10; // UTC is 10 hours ahead of HST
+  if (utcHour >= 24) {
+    utcHour -= 10;
+  }
+  const hourStr = utcHour.toString().padStart(2, "0");
+  return hourStr;
+};
 
 const createSVG = async () => {
   const spec = {
@@ -7,58 +19,148 @@ const createSVG = async () => {
     description: "Line chart.",
     data: {
       values: [
-        { dateTime: "07/14/2023 14:59:41.973441437", value: 28, subplot: "wx" },
-        { dateTime: "07/14/2023 15:02:21.973435037", value: 55, subplot: "wx" },
-        { dateTime: "07/14/2023 16:03:41.973431837", value: 43, subplot: "wx" },
         {
-          dateTime: "07/14/2023 17:05:01.973428637",
+          dateTime: "2023-07-14T14:59:41.973441437",
+          value: 28,
+          subplot: "wx",
+          label: "hello",
+        },
+        {
+          dateTime: "2023-07-14T15:02:21.973435037",
+          value: 55,
+          subplot: "wx",
+          label: "world",
+        },
+        {
+          dateTime: "2023-07-14T16:03:41.973431837",
+          value: 43,
+          subplot: "wx",
+          label: "message",
+        },
+        {
+          dateTime: "2023-07-14T17:05:01.973428637",
           value: 91,
           subplot: "nama",
+          label: "hello",
         },
         {
-          dateTime: "07/14/2023 18:06:21.973425437",
+          dateTime: "2023-07-14T18:06:21.973425437",
           value: 81,
           subplot: "nama",
+          label: "world",
         },
         {
-          dateTime: "07/14/2023 19:07:41.973422237",
+          dateTime: "2023-07-14T19:07:41.973422237",
           value: 53,
           subplot: "nama",
+          label: "message",
         },
         {
-          dateTime: "07/14/2023 20:09:01.973419037",
+          dateTime: "2023-07-14T20:09:01.973419037",
           value: 19,
           subplot: "nama",
+          label: "hello",
         },
         {
-          dateTime: "07/14/2023 21:10:21.973415837",
+          dateTime: "2023-07-14T21:10:21.973415837",
           value: 87,
           subplot: "nama",
+          label: "world",
         },
         {
-          dateTime: "07/14/2023 22:11:41.973412637",
+          dateTime: "2023-07-14T22:11:41.973412637",
           value: 52,
           subplot: "nama",
+          label: "message",
         },
       ],
     },
+    transform: [
+      {
+        calculate: "utc(datum.dateTime)",
+        as: "utcDateTime",
+      },
+      {
+        calculate: "hours(datum.utcDateTime)",
+        as: "utcHour",
+      },
+      // {
+      //   calculate: "datum.value - 10",
+      //   as: "value",
+      // },
+    ],
     facet: {
-      row: { field: "subplot", type: "nominal" },
+      row: {
+        field: "subplot",
+        type: "nominal",
+        header: {
+          title: { field: "subplot", type: "nominal" },
+          labelFontSize: 11,
+          labelFontWeight: "bold",
+          labelPadding: 0,
+        },
+      },
     },
     spec: {
-      width: 800,
-      mark: "line",
-      encoding: {
-        x: {
-          field: "dateTime",
-          type: "temporal",
-          scale: {
-            domain: ["07/14/2023 14:00:00.000", "07/15/2023 14:00:00.000"],
+      layer: [
+        {
+          width: 800,
+          mark: "point",
+          encoding: {
+            x: {
+              field: "dateTime",
+              type: "temporal",
+              scale: {
+                domain: ["07/14/2023 14:00:00.000", "07/15/2023 14:00:00.000"],
+              },
+              axis: {
+                format: "%H",
+                title: "HST",
+                titlePadding: 10,
+              },
+            },
+            y: { field: "value", type: "quantitative", axis: { title: null } },
+            color: {
+              field: "label",
+              type: "nominal",
+              scale: {
+                range: [
+                  "#1f77b4", // blue
+                  "#ff7f0e", // orange
+                  "#2ca02c", // green
+                  "#d62728", // red
+                  "#9467bd", // purple
+                  "#8c564b", // brown
+                  "#e377c2", // pink
+                  "#7f7f7f", // gray
+                  "#bcbd22", // yellow-green
+                  "#17becf", // teal
+                  "#f74e8f", // magenta
+                  "#9c9ede", // light blue
+                ],
+              },
+            },
           },
-          axis: { format: "%H" },
         },
-        y: { field: "value", type: "quantitative" },
-      },
+        // // transparent layer for UTC axis labels
+        // {
+        //   mark: { type: "line", opacity: 0 },
+        //   encoding: {
+        //     x: {
+        //       field: "dateTime",
+        //       type: "temporal",
+        //       axis: {
+        //         format: "%H",
+        //         title: "UTC",
+        //         titlePadding: 10,
+        //       },
+        //       // timeUnit: "yearmonthdatehoursminutes",
+        //       // scale: { type: "utc" },
+        //     },
+        //   },
+        // },
+      ],
+      resolve: { axis: { x: "independent" } },
     },
   } as unknown as lite.TopLevelSpec;
 
@@ -71,12 +173,13 @@ const createSVG = async () => {
 const VegaChart = async () => {
   try {
     const svgStr = await createSVG();
-    const dateStr = new Date().toISOString();
+    const date = new Date();
+    const timeStr = date.getHours() + ":" + date.getMinutes();
 
     return (
       <div>
+        <p>{timeStr}</p>
         <img src={`data:image/svg+xml;utf8,${encodeURIComponent(svgStr)}`} />
-        <p>{dateStr}</p>
       </div>
     );
   } catch (e) {
--- tonight/src/constants/figureGroups.ts
+++ tonight/src/constants/figureGroups.ts
@@ -2,16 +2,16 @@ type Subplot = { [key: string]: string[] }; // {y-axis label: PV variables}
 type groups = Subplot[]; // array of Groups, each group is one set of PV variables to plot on same axes (a subplot)
 
 export const jcmtwx: groups = [
-  { temperature: ["ws:wxt510:stat:airTemp"] },
-  { humidity: ["ws:wxt510:stat:humidity"] },
-  { pressure: ["ws:wxt510:stat:pressure"] },
-  { "wind speed": ["ws:wxt510:stat:windSpd"] },
-  { "wind direction": ["ws:wxt510:stat:windDir"] },
+  { Temperature: ["ws:wxt510:stat:airTemp"] },
+  { Humidity: ["ws:wxt510:stat:humidity"] },
+  { Pressure: ["ws:wxt510:stat:pressure"] },
+  { "Wind speed": ["ws:wxt510:stat:windSpd"] },
+  { "Wind direction": ["ws:wxt510:stat:windDir"] },
 ];
 
 export const jcmtnama: groups = [
   {
-    "temperature (k)": [
+    "Temperature (k)": [
       "nmnCryo:ls:temp1",
       "nmnCryo:ls:temp2",
       "nmnCryo:ls:temp3",
