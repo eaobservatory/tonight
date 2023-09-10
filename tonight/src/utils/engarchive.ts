27ee4079f65a7c971a86d930782615e7d5a7edaf
katiejl <katieliu2@live.com>
2023-09-10 18:09:55 -0400

    more efficient data cleaning

--- tonight/src/utils/engarchive.ts
+++ tonight/src/utils/engarchive.ts
@@ -34,9 +34,9 @@ export const getPV = cache(async (pv: PV, date = "live") => {
     const data = res
       .split("\n") // split response into lines
       .slice(18, -1); // remove header
-    cleanPVData(data, pv);
+    const cleanData = cleanPVData(data, pv);
     console.log(`${pv}: success!`);
-    return { label: labels[pv], data: data };
+    return { label: labels[pv], data: cleanData };
   } catch (e) {
     console.log(`${pv}: ERROR -- ${(e as Error).message}`);
     return []; // plot should be blank if error occurs
@@ -44,37 +44,43 @@ export const getPV = cache(async (pv: PV, date = "live") => {
 });
 
 const cleanPVData = (data: string[], pv: PV) => {
-  // remove #N/A and lines whose values start with a space
-  for (let i = data.length - 1; i >= 0; i--) {
-    const line = data[i];
+  const cleanData = [];
+  for (const line of data) {
+    let add = true;
+    const point = line.split("\t");
+
+    // filter out #N/A and lines whose values start with a space
     if (
       line.endsWith("#N/A") ||
       (line.split("\t")[1] && line.split("\t")[1].startsWith(" "))
     ) {
-      data.splice(i, 1);
+      add = false;
     }
-  }
 
-  // remove 0s
-  const removeZeroesList: PV[] = [
-    "ws:wxt510:stat:airTemp",
-    "ws:wxt510:stat:pressure",
-    "ws:wxt510:stat:humidity",
-    "scu2CCS:ls370c:chan:k",
-  ];
-
-  const removeZeroes = (data: string[]) => {
-    for (let i = 0; i < data.length; i++) {
-      const line = data[i];
-      const point = line.split("\t");
+    // filter out 0s
+    const removeZeroesList: PV[] = [
+      "ws:wxt510:stat:airTemp",
+      "ws:wxt510:stat:pressure",
+      "ws:wxt510:stat:humidity",
+      "scu2CCS:ls370c:chan:k",
+    ];
+    if (removeZeroesList.includes(pv)) {
       if (Number(point[1]) == 0) {
-        data.splice(i, 1);
-        i--;
+        add = false;
       }
     }
-  };
 
-  if (removeZeroesList.includes(pv)) {
-    removeZeroes(data);
+    // filter out 999.999900
+    if (pv == "scu2CCS:ls370a:chan1:k") {
+      if (Number(point[1]) == 999.9999) {
+        add = false;
+      }
+    }
+
+    if (add) {
+      cleanData.push(line);
+    }
   }
+
+  return cleanData;
 };
